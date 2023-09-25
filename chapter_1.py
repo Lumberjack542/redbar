8b6ceb69224b23dc7c035b761ad897c38972f1b7
Vlad Mironchik <542avy3@gmail.com>
2023-09-25 18:09:42 +0300

    chapter 1 and 2

---
+++ chapter_1.py
@@ -0,0 +1,52 @@
+import cv2
+import os
+import argparse
+
+
+def extract_frames(video_path, num_frames1, num_frames2, output_folder):
+
+    cap = cv2.VideoCapture(video_path)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    print(width, 'x', height)  
+    
+    
+    if not cap.isOpened():
+        print("Error: Could not open video file.")
+        exit()
+
+    
+    os.makedirs(output_folder, exist_ok=True)
+
+    frame_count = 0
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break 
+
+        frame_count += 1
+        frame_filename = os.path.join(output_folder, f"frame_{frame_count:04d}.jpg")
+        if num_frames2 >= frame_count >= num_frames1:
+            cv2.imwrite(frame_filename, frame)
+        
+   
+    print(f"{frame_count} frames extracted and saved to {output_folder}")
+
+    
+    cap.release()
+    cv2.destroyAllWindows()
+   
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extract frames from a video.")
+    parser.add_argument("video_path", type=str, help="Path to the input video file.")
+    parser.add_argument("num_frames1", type=int, help="Number 1 of frames to extract.")
+    parser.add_argument("num_frames2", type=int, help="Number 2 of frames to extract.")
+    parser.add_argument("output_folder", type=str, help="Path to save the extracted frames.")
+
+    args = parser.parse_args()
+
+    extract_frames(args.video_path, args.num_frames1, args.num_frames2,  args.output_folder)
---
+++ chapter_2.py
@@ -0,0 +1,31 @@
+from scenedetect import open_video, SceneManager, split_video_ffmpeg, VideoManager
+from scenedetect.detectors import ContentDetector
+from scenedetect.video_splitter import split_video_ffmpeg
+import os
+import scenedetect.video_splitter as video_splitter
+import argparse
+
+
+def split_video_into_scenes(video_path, output_folder, threshold=27.0):
+
+    video =  VideoManager([video_path])
+    scene_manager = SceneManager()
+    scene_manager.add_detector(
+         ContentDetector(threshold=threshold))
+    scene_manager.detect_scenes(video, show_progress=True)
+    scene_list = scene_manager.get_scene_list()
+    
+    os.mkdir(output_folder, exist_ok=True)
+    video_splitter.split_video_ffmpeg([video_path], scene_list, output_folder + "/" + '$VIDEO_NAME-$SCENE_NUMBER' +".mp4", os.path.basename(output_folder) ,suppress_output=True)
+    
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extract frames from a video.")
+    parser.add_argument("video_path", type=str)
+    parser.add_argument("output_folder", type=str)
+
+    args = parser.parse_args()
+
+    split_video_into_scenes(args.video_path,  args.output_folder)
